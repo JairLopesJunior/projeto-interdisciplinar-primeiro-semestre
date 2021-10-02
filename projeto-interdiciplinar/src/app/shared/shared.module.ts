6a78a5eb8ad94693fe3b3ded7d85c5fd5ca4dca4
unknown <jair.lopes@fatec.sp.gov.br>
2021-10-02 16:29:24 -0300

    Criado o projeto utilizando o Angular/CLI - Cirado o componente NavBar

---
+++ projeto-interdiciplinar/src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    NavbarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
