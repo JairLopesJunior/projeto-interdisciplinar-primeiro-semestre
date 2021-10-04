17c277ce3424c810dcea34a15501e1d5d1a8ede2
unknown <jair.lopes@fatec.sp.gov.br>
2021-10-03 23:38:21 -0300

    Criado os inputs do component Card

--- projeto-interdiciplinar/src/app/shared/pages/home/home.component.ts
+++ projeto-interdiciplinar/src/app/shared/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  teste: string = 'sdfsd';
+  titulo: string = 'Tittulo2';
+  imagem: string = 'assets/imagem.png';
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  perfis = [
+    {
+      imagem: 'assets/imagem.png',
+      descricao: 'Teste1',
+      titulo: 'titulo1'
+    },
+    {
+      imagem: 'assets/imagem.png',
+      descricao: 'Teste2',
+      titulo: 'titulo2'
+    },
+    {
+      imagem: 'assets/imagem.png',
+      descricao: 'Teste3',
+      titulo: 'titulo3'
+    },
+    {
+      imagem: 'assets/imagem.png',
+      descricao: 'Teste4',
+      titulo: 'titulo4'
+    }
+  ]
 
 }
