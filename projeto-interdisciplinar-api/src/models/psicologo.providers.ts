55de5155f53b8f4524c16566841273571c7c42d0
unknown <jair.lopes@fatec.sp.gov.br>
2021-10-17 17:38:36 -0300

    Criado a Entidade Psicologo e mapeado seus campos

---
+++ projeto-interdisciplinar-api/src/models/psicologo.providers.ts
@@ -0,0 +1,9 @@
+/*import { Connection } from 'typeorm';
+
+export const psicologoProviders = [
+    {
+        provide: 'PSICOLOGO_REPOSITORY',
+        useFactory: (connection: Connection) => connection.getRepository(Psicologo),
+        inject: ['DATABASE_CONNECTION'],
+    },
+];*/
