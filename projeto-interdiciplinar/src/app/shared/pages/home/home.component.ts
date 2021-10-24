7a4cba40cca615d1a2480816e9496d7c220b0388
unknown <jair.lopes@fatec.sp.gov.br>
2021-10-24 19:06:10 -0300

    Adicionado Card a Home do site

--- projeto-interdiciplinar/src/app/shared/pages/home/home.component.ts
+++ projeto-interdiciplinar/src/app/shared/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Psicologo } from 'src/app/models/psicologo';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  psicologos: Psicologo[] = [];
 
   constructor() { }
 
