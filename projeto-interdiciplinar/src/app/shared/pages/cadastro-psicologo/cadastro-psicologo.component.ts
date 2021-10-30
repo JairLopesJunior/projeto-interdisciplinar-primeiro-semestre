b0c2b664381a0d8ae1563e5f21038b1ae13509d8
unknown <jair.lopes@fatec.sp.gov.br>
2021-10-30 18:43:14 -0300

    Finalizado a tela de Psicologo

--- projeto-interdiciplinar/src/app/shared/pages/cadastro-psicologo/cadastro-psicologo.component.ts
+++ projeto-interdiciplinar/src/app/shared/pages/cadastro-psicologo/cadastro-psicologo.component.ts
@@ -1,3 +1,4 @@
+import { CidadeEstadoService } from './../cadastro/cidade-estado.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
@@ -10,7 +11,8 @@ import { PsicologoService } from './psicologo.service';
 })
 export class CadastroPsicologoComponent implements OnInit {
 
-  hasEstado = false;
+  estados = this.cidadeEstadoService.allEstados();
+  cidades: Array<any>;
 
   experiencias = [
       { experiencia: 'Adolescência' },
@@ -36,15 +38,21 @@ export class CadastroPsicologoComponent implements OnInit {
 
   constructor(private fb: FormBuilder,
               private _psicologoService: PsicologoService,
-              private _router: Router) {}
+              private _router: Router,
+              private cidadeEstadoService: CidadeEstadoService) {}
 
   ngOnInit(): void {
     this.formularioCliente();
-    this.VerificarEstado();
   }
 
   onSubmit(): void {
     if(this.cadastroPsicologo.valid) {
+      const idEstado = this.cadastroPsicologo.get('estado');
+      this.cidadeEstadoService.allEstados().filter( (estadoFilter) => {
+        if(idEstado?.value === estadoFilter.ID) {
+          idEstado?.patchValue(estadoFilter.Nome);
+        }
+      });
       this._psicologoService.save(this.cadastroPsicologo.value).subscribe({
         next: psicologo => {
           alert("Salvo com sucesso.")
@@ -117,7 +125,7 @@ export class CadastroPsicologoComponent implements OnInit {
           Validators.maxLength(100)
         ])
       ],
-      estado: [{value: ''}, Validators.compose([
+      estado: ['', Validators.compose([
           Validators.required
         ])
       ],
@@ -137,148 +145,16 @@ export class CadastroPsicologoComponent implements OnInit {
     })
   }
 
-  private VerificarEstado(): void {
+  hasEstado(): void {
     const estado = this.cadastroPsicologo.get('estado')?.value;
-    console.log(estado)
-    if(estado) {
+    if(estado || estado !== '') {
       this.cadastroPsicologo.controls['cidade'].enable();
+      console.log(typeof estado)
+      const cidadesEncontradas = this.cidadeEstadoService.getCidadesPeloIdEstado(estado);
+      this.cidades = cidadesEncontradas;
+      return;
     }
+    this.cadastroPsicologo.controls['cidade'].disable();
   }
-
-  estados = [{
-    "ID": "1",
-    "Sigla": "AC",
-    "Nome": "Acre"
-  },
-       {
-    "ID": "2",
-    "Sigla": "AL",
-    "Nome": "Alagoas"
-  },
-       {
-    "ID": "3",
-    "Sigla": "AM",
-    "Nome": "Amazonas"
-  },
-       {
-    "ID": "4",
-    "Sigla": "AP",
-    "Nome": "Amapá"
-  },
-       {
-    "ID": "5",
-    "Sigla": "BA",
-    "Nome": "Bahia"
-  },
-       {
-    "ID": "6",
-    "Sigla": "CE",
-    "Nome": "Ceará"
-  },
-       {
-    "ID": "7",
-    "Sigla": "DF",
-    "Nome": "Distrito Federal"
-  },
-       {
-    "ID": "8",
-    "Sigla": "ES",
-    "Nome": "Espírito Santo"
-  },
-       {
-    "ID": "9",
-    "Sigla": "GO",
-    "Nome": "Goiás"
-  },
-       {
-    "ID": "10",
-    "Sigla": "MA",
-    "Nome": "Maranhão"
-  },
-       {
-    "ID": "11",
-    "Sigla": "MG",
-    "Nome": "Minas Gerais"
-  },
-       {
-    "ID": "12",
-    "Sigla": "MS",
-    "Nome": "Mato Grosso do Sul"
-  },
-       {
-    "ID": "13",
-    "Sigla": "MT",
-    "Nome": "Mato Grosso"
-  },
-       {
-    "ID": "14",
-    "Sigla": "PA",
-    "Nome": "Pará"
-  },
-       {
-    "ID": "15",
-    "Sigla": "PB",
-    "Nome": "Paraíba"
-  },
-       {
-    "ID": "16",
-    "Sigla": "PE",
-    "Nome": "Pernambuco"
-  },
-       {
-    "ID": "17",
-    "Sigla": "PI",
-    "Nome": "Piauí"
-  },
-       {
-    "ID": "18",
-    "Sigla": "PR",
-    "Nome": "Paraná"
-  },
-       {
-    "ID": "19",
-    "Sigla": "RJ",
-    "Nome": "Rio de Janeiro"
-  },
-       {
-    "ID": "20",
-    "Sigla": "RN",
-    "Nome": "Rio Grande do Norte"
-  },
-       {
-    "ID": "21",
-    "Sigla": "RO",
-    "Nome": "Rondônia"
-  },
-       {
-    "ID": "22",
-    "Sigla": "RR",
-    "Nome": "Roraima"
-  },
-       {
-    "ID": "23",
-    "Sigla": "RS",
-    "Nome": "Rio Grande do Sul"
-  },
-       {
-    "ID": "24",
-    "Sigla": "SC",
-    "Nome": "Santa Catarina"
-  },
-       {
-    "ID": "25",
-    "Sigla": "SE",
-    "Nome": "Sergipe"
-  },
-       {
-    "ID": "26",
-    "Sigla": "SP",
-    "Nome": "São Paulo"
-  },
-       {
-    "ID": "27",
-    "Sigla": "TO",
-    "Nome": "Tocantins"
-  }]
 
 }
